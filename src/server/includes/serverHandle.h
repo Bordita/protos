cb1f3912cc57f0ef02b9bd130ab8798402bf223a
Tomas Borda <tborda@itba.edu.ar>
2025-07-14 11:12:00 -0300

    fix: listen localhost in ipv4 and ipv6 by default

--- src/server/includes/serverHandle.h
+++ src/server/includes/serverHandle.h
@@ -5,9 +5,12 @@
 #define INITIAL_SELECTOR_ITEMS 20
 #define LISTEN_MAX_QUEUE 50
 #define MAX_FDS 1023
-#define SERVER_LISTEN_SOCKET_COUNT 2
+#define SERVER_LISTEN_SOCKET_COUNT 4
 #define FDS_PER_SOCKS_CONNECTION 2
 #define FDS_RESERVED_BY_OS 2
+#define LOCALHOST_ADDR_IPV4 "127.0.0.1"
+#define LOCALHOST_ADDR_IPV6 "::1"
+
 
 int server_handler(char * socks_addr, char * socks_port, char * mng_addr,char * mng_port);
 
